a2a96a7338b66c0174516667c8101c4f2b9e53f9
Philipp Stracker <info@philippstracker.com>
2025-11-06 20:59:14 +0100

    🚚 Apply PSR-4 naming conventions
    The “class-“ prefix is a legacy decision which is fighting modern development practices. I decide to upgrade the codebase to modern file- and folder-names

--- init.php
+++ init.php
@@ -4,15 +4,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-require_once __DIR__ . '/src/exceptions/class-wpdi-exception.php';
-require_once __DIR__ . '/src/exceptions/class-container-exception.php';
-require_once __DIR__ . '/src/exceptions/class-not-found-exception.php';
-require_once __DIR__ . '/src/exceptions/class-circular-dependency-exception.php';
-require_once __DIR__ . '/src/class-auto-discovery.php';
-require_once __DIR__ . '/src/class-compiler.php';
-require_once __DIR__ . '/src/class-container.php';
-require_once __DIR__ . '/src/class-scope.php';
+require_once __DIR__ . '/src/Exceptions/Wpdi_Exception.php';
+require_once __DIR__ . '/src/Exceptions/Container_Exception.php';
+require_once __DIR__ . '/src/Exceptions/Not_Found_Exception.php';
+require_once __DIR__ . '/src/Exceptions/Circular_Dependency_Exception.php';
+require_once __DIR__ . '/src/Auto_Discovery.php';
+require_once __DIR__ . '/src/Compiler.php';
+require_once __DIR__ . '/src/Container.php';
+require_once __DIR__ . '/src/Scope.php';
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
-	require_once __DIR__ . '/src/commands/class-compile-command.php';
+	require_once __DIR__ . '/src/Commands/Compile_Command.php';
 }
