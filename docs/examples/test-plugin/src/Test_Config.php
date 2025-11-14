743a17d41b0eca78b2616813a353cd3c7b36e329
Philipp Stracker <info@philippstracker.com>
2025-11-14 20:29:22 +0100

    📝 Update docs with recommended usage samples

--- docs/examples/test-plugin/src/Test_Config.php
+++ docs/examples/test-plugin/src/Test_Config.php
@@ -1,13 +1,14 @@
 <?php
 
 class Test_Config {
-	private string $environment;
-
-	public function __construct() {
-		$this->environment = wp_get_environment_type();
-	}
-
-	public function get_environment() : string {
-		return $this->environment;
+	/**
+	 * Get WordPress environment type.
+	 *
+	 * Note: This calls wp_get_environment_type() on each access to get fresh value.
+	 * Since this service is a singleton, storing the value in a property would cache
+	 * it at instantiation, which is incorrect for WordPress option/config values.
+	 */
+	public function get_environment(): string {
+		return wp_get_environment_type();
 	}
 }
